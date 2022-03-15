e0e316743b1b696bbf9f30adac916bb31fdc3ee0
github-kits26kat24 <100363986+github-kits26kat24@users.noreply.github.com>
2022-03-15 00:18:50 +0000

    Update security.group.tf

--- security.group.tf
+++ security.group.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "sg" {
   name        = "sg"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_security_group.sg.id
+  #vpc_id      = aws_security_group.sg.id
 
 
   ingress {
