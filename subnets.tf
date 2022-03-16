aa667d2aa8c9f3b459e28c0f5ce9a2ccbb8fe0ab
Kits26kat24 <kay.tijani@gmail.com>
2022-03-16 14:06:33 +0000

    working on the 2 private and a public ip addresses assignment

--- subnets.tf
+++ subnets.tf
@@ -4,7 +4,34 @@ resource "aws_subnet" "public_subnet_2a" {
   availability_zone = "eu-west-2a"
   map_public_ip_on_launch = "true"
   tags = {
-    Name = "kitskat"
+    Name = "kitskat 1"
   }
 }
 
+resource "aws_subnet" "public_subnet_1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-west-2a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "kitskat 2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name = "London_City"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "eu-west-2a"
+  tags = {
+    Name = "Lagos_City"
+  }
+}
